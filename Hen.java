2acfcf959a00754be58a51300d722b4920eaeb78
jvmNet <jvm.dot@gmail.com>
2017-12-18 14:33:49 +0200

    Create Interface_ENUM AbstractClass

---
+++ Hen.java
@@ -0,0 +1,10 @@
+package factoryHen;
+
+public abstract class Hen {
+
+    public abstract int getCountOfEggsPerMonth();
+
+    public String getDescription() {
+        return "Я - курица.";
+    }
+}
